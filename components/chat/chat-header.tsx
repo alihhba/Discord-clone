6f6a19b2606350ed7ff8ae12e34e00a1f8d950de
alireza hb <ahbhbhba@gmail.com>
2023-11-13 13:40:56 +0330

    ...

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import MenuMobileTrigger from "../menu-mobileTrigger";
 import { Hash, Mic, Video } from "lucide-react";
 import { ChannelType } from "@prisma/client";
+import { AvatarImage } from "../ui/avatar";
+import UserAvatar from "../user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -17,12 +19,19 @@ const iconMap = {
   [ChannelType.VIDEO]: <Video className="h-4 w-4 mt-0.5" />,
 };
 
-const ChatHeader = ({ serverId, name, type, channelType }: ChatHeaderProps) => {
+const ChatHeader = ({
+  serverId,
+  name,
+  type,
+  channelType,
+  imageUrl,
+}: ChatHeaderProps) => {
   return (
     <div className="flex w-full h-12 border-b border-r-zinc-500 items-center gap-3">
       <MenuMobileTrigger serverId={serverId} />
       <div className="flex items-center gap-1">
         {channelType && type === "channel" && iconMap[channelType]}
+        {type === "conversation" && <UserAvatar src={imageUrl} className="w-8 h-8 mr-2" />}
         <p className="font-semibold text-md text-black dark:text-white">
           {name}
         </p>
